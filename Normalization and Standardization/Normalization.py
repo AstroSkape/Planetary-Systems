622923bb0feb6b51b5eacedd80e3bc905bdfa840
Sharan2001 <56764785+Sharan2001@users.noreply.github.com>
2020-11-03 01:57:14 +0530

    Add files via upload

--- Normalization and Standardization/Normalization.py
+++ Normalization and Standardization/Normalization.py
@@ -1,14 +1,13 @@
 from sklearn import preprocessing
-#import numpy as np
 import pandas as pd
 
-df = pd.read_csv(r'D:\Engineering\Sem - 3\Statistics for Data Science\SDS Project\Normalization and Standardization\Mean and Variance.csv')
+df = pd.read_csv(r'D:\Engineering\Sem - 3\Statistics for Data Science\SDS Project\Normalization and Standardization\Mean and Variance.csv') # Importng csv file  
 
 a=list(df.columns)
 a.remove('Unnamed: 0')
 
 df2=df.transpose()
-df2.drop(["Unnamed: 0"], inplace = True)
+df2.drop(["Unnamed: 0"], inplace = True) #removing an overlapping row
 
 normalized = preprocessing.normalize(df2)
 df3=normalized.transpose()
@@ -18,8 +17,10 @@ x=0
 for i in a:
     df3.rename(columns={x:i}, inplace=True)
     x=x+1
+    
 df3.rename(index={0:'Normalized Mean'}, inplace=True)
 df3.rename(index={1:'Normalized Variance'}, inplace=True)
-df3.to_csv(r'D:\Engineering\Sem - 3\Statistics for Data Science\SDS Project\Normalization and Standardization\Mean and Variance - Normalized.csv') #inplace of "D:\Engineering\Sem - 3\Statistics for Data Science\SDS Project\cleaned-without missing.csv", add the directory of the folder you want ot save the CSV in
 
-df3.to_csv('Mean and Variance.csv',mode = 'a', header = False) #Writing to same s=csv file
+df3.to_csv(r'D:\Engineering\Sem - 3\Statistics for Data Science\SDS Project\Normalization and Standardization\Mean and Variance - Normalized.csv') # Creating new csv file for referencec
+
+df3.to_csv('Mean and Variance.csv',mode = 'a', header = False) # Writing to same csv file
